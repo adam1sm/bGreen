4f4bb59b1c26c089d5a3160b211cb6d9f65c5512
adam1sm <adam.khadre@gmail.com>
2025-10-25 21:36:46 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${inter.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
